5a0cd43fa3c0026d7ae61aa1c9c5b39d4fc33239
0186000213 <0186000213@zwxict.com>
2021-06-25 16:44:51 +0800

    test

---
+++ tests/unit/datepicker.spec.js
@@ -0,0 +1,65 @@
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+
+import Datepicker from '@/DatePicker/HotelDatePicker.vue'
+
+describe('Datepicker Calendar', () => {
+  const wrapper = mount(Datepicker)
+
+  it('should correctly re-render the calendar', () => {
+    expect(wrapper.vm.value).to.equal(true)
+    wrapper.vm.reRender()
+    expect(wrapper.vm.isOpen).to.equal(false)
+    expect(wrapper.vm.value).to.equal(true)
+
+    setTimeout(() => {
+      expect(wrapper.vm.isOpen).to.equal(true)
+      expect(wrapper.vm.value).to.equal(true)
+    }, 200)
+  })
+})
+
+describe('Datepicker Component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(Datepicker, {
+      attachToDocument: true,
+      propsData: {
+        minNights: 3,
+        disabledDates: ['2020-05-28', '2020-05-10', '2020-05-01', '2020-05-22'],
+      },
+    })
+  })
+
+  it('should toggle the calendar visibility on input click', () => {
+    expect(wrapper.vm.isOpen).to.equal(false)
+
+    const datepickerInput = wrapper.find('[data-qa="vhd__datepickerInput"]')
+
+    datepickerInput.trigger('click')
+
+    expect(wrapper.vm.isOpen).to.equal(true)
+  })
+
+  it('should correctly render the next and previous months', () => {
+    const { activeMonthIndex } = wrapper.vm
+
+    wrapper.vm.renderNextMonth()
+    expect(wrapper.vm.activeMonthIndex).to.equal(activeMonthIndex + 1)
+
+    wrapper.vm.renderPreviousMonth()
+    expect(wrapper.vm.activeMonthIndex).to.equal(activeMonthIndex)
+  })
+
+  // it('should correctly parse and sort the disabled dates', () => {
+  //   // wrapper.vm.parseDisabledDates()
+  //   wrapper.vm.createHalfDayDates(wrapper.vm.baseHalfDayDates)
+  //   expect(wrapper.vm.sortedDisabledDates).to.eql([
+  //     new Date('2020-05-01'),
+  //     new Date('2020-05-10'),
+  //     new Date('2020-05-22'),
+  //     new Date('2020-05-28'),
+  //   ])
+  // })
+})
